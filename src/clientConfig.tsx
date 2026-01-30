a635915e889b18903da462a505879b62cad2d43f
FlorianMon <98278460+FlorianMon@users.noreply.github.com>
2026-01-30 17:07:51 +0100

    Add files via upload

---
+++ src/clientConfig.tsx
@@ -0,0 +1,5 @@
+export const clientConfig = {
+    clientId: '8deadb75-3c78-401a-a1d2-2216208b6434',
+    redirectUri: 'http://localhost:5173',
+    offlinePresenceId: 'ccf3c10a-aa2c-4845-8e8d-f59fa48c58e5',
+}
