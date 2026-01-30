476acf37bcf1cc61c067aecd2a08e5dd808b9524
FlorianMon <98278460+FlorianMon@users.noreply.github.com>
2026-01-30 18:24:13 +0100

    Fix formatting of clientConfig export

--- src/clientConfig.tsx
+++ src/clientConfig.tsx
@@ -1,6 +1,8 @@
+
 export const clientConfig = {
     clientId: '8deadb75-3c78-401a-a1d2-2216208b6434',
     redirectUri: 'https://florianmon.github.io/GenesysTestApiOvalo/',
     offlinePresenceId: 'ccf3c10a-aa2c-4845-8e8d-f59fa48c58e5',
 }
+
 
